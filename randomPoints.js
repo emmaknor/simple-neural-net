d5647415ce3b173c2088df58519f7ae0fe35cf1a
Emma <knoremma@gmail.com>
2021-05-06 19:16:11 -0600

    add comment denoting x and y coordinates

--- randomPoints.js
+++ randomPoints.js
@@ -3,4 +3,5 @@ R = require('ramda');
 let rand = (high, low) => Math.random() * (high - low) + low;
 
 // create a range from 0 to 1 and generate a random number from -1 to 1
+// each element w/in random points will represent an x and y coordinate
 let randomPoints = R.range(0, 100).map(() => rand(-1, 1));
